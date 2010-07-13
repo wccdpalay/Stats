603cfb2141eded21e7ab36528cc48ca5a2ce7d8d
dpalay <dpalay@wccnet.org>
2010-07-13 12:11:17 -0400

    css changes

--- app/controllers/joomla_controller.rb
+++ app/controllers/joomla_controller.rb
@@ -1,23 +1,24 @@
 class JoomlaController < ApplicationController
   before_filter :check_user, :except => [:debug, :kick]
-  
-  
-  def index  
+
+
+  def index
     @content = get_joomla_session_id
-    
+
     @session = Jsession.find_by_session_id(@content)
     @user = User.find_by_username(@session.username) unless @session.nil?
   end
-  
+
   def kick
-    @message = "Your session has either expired or never existed.  
-    Please go back and log into Serafina's main page"
+    @message = "Your session has either expired or never existed.
+    Please go back and refresh Serafina's main page"
   end
-  
+
   def debug
     @user = get_user(session[:user])
     @result_from_php = JSESSION_NAME
     @cookie_contents = cookies[@result_from_php]
     @jsessions = Jsession.find(:all)
+    params[:error] ||= "Your session has timed out.  Please refresh the front page of serafina (The Joomla Page) and try again."
   end
 end
